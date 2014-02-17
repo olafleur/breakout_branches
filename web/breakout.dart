e03bc33ad5e2ad9f47f5f1e84772e7a15bf18700
Olivier Lafleur <Olivier.Lafleur@cll.qc.ca>
2014-02-17 16:12:21 -0500

    breakout02

--- web/breakout.dart
+++ web/breakout.dart
@@ -1,16 +1,16 @@
 import 'dart:html';
+import 'dart:math';
 
 void main() {
-  querySelector("#sample_text_id")
-    ..text = "Click me!"
-    ..onClick.listen(reverseText);
+  // get a reference to the canvas
+  var canvas = querySelector('#canvas');
+  var ctx = canvas.getContext("2d");
+
+  // draw a circle
+  ctx.beginPath();
+  ctx.arc(75, 75, 10, 0, PI * 2, true);
+  ctx.closePath();
+  ctx.fill();
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector("#sample_text_id").text = buffer.toString();
-}
+
