16f123d3adebfa7e3cf5339195092077459962bf
Olivier Lafleur <Olivier.Lafleur@cll.qc.ca>
2014-02-17 16:27:37 -0500

    breakout06

---
+++ lib/breakout.dart
@@ -0,0 +1,53 @@
+library breakout;
+
+import 'dart:html';
+import 'dart:math';
+
+var WHITE = 'ffffff';
+var BLACK = '000000';
+var WIDTH;
+var HEIGHT;
+
+var x = 150;
+var y = 150;
+var dx = 2;
+var dy = 4;
+var context;
+
+init() {
+  CanvasElement canvas = querySelector('#canvas');
+  WIDTH = canvas.width;
+  HEIGHT = canvas.height;
+  context = canvas.getContext("2d");
+}
+
+rectangle(x, y, w, h, color) {
+  context
+    ..fillStyle = color
+    ..beginPath()
+    ..rect(x, y, w, h)
+    ..closePath()
+    ..fill();
+}
+
+circle(x, y, r, color) {
+  context
+    ..fillStyle = color
+    ..beginPath()
+    ..arc(x, y, r, 0, PI * 2, true)
+    ..closePath()
+    ..fill();
+}
+
+border() {
+  context
+    ..beginPath()
+    ..rect(0, 0, WIDTH, HEIGHT)
+    ..closePath()
+    ..stroke();
+}
+
+clear() {
+  context.clearRect(0, 0, WIDTH, HEIGHT);
+  border();
+}
--- web/breakout.dart
+++ web/breakout.dart
@@ -1,23 +1,9 @@
+import 'package:breakout/breakout.dart';
+
 import 'dart:html';
-import 'dart:math';
 
-// begin library code
-var WHITE = 'ffffff';
-var BLACK = '000000';
-var WIDTH;
-var HEIGHT;
-
-var x = 150;
-var y = 150;
-var dx = 2;
-var dy = 4;
-var ctx;
-
-init() {
-  CanvasElement canvas = querySelector('#canvas');
-  WIDTH = canvas.width;
-  HEIGHT = canvas.height;
-  ctx = canvas.getContext("2d");
+start() {
+  init();
   // redraw
   window.animationFrame.then(gameLoop);
 }
@@ -27,45 +13,19 @@ gameLoop(num delta) {
   window.animationFrame.then(gameLoop);
 }
 
-rect(x, y, w, h, color) {
-  ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.rect(x, y, w, h);
-  ctx.closePath();
-  ctx.fill();
-}
-
-circle(x, y, r, color) {
-  ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, PI * 2, true);
-  ctx.closePath();
-  ctx.fill();
-}
-
-border() {
-  ctx.beginPath();
-  ctx.rect(0, 0, WIDTH, HEIGHT);
-  ctx.closePath();
-  ctx.stroke();
-}
-
-clear() {
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  border();
-}
-// end library code
-
 draw() {
   clear();
   circle(x, y, 10, BLACK);
+
+  if (x + dx > WIDTH || x + dx < 0) dx = -dx;
+  if (y + dy > HEIGHT || y + dy < 0) dy = -dy;
 
   x += dx;
   y += dy;
 }
 
 main() {
-  init();
+  start();
 }
 
 
