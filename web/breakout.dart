d4e9e11a1164260e63e116de97a95b8c9f7a5f84
Olivier Lafleur <Olivier.Lafleur@cll.qc.ca>
2014-02-17 16:15:14 -0500

    breakout03

--- web/breakout.dart
+++ web/breakout.dart
@@ -7,8 +7,22 @@ void main() {
   var ctx = canvas.getContext("2d");
 
   // draw a circle
+  ctx.fillStyle = "#00a308";
   ctx.beginPath();
-  ctx.arc(75, 75, 10, 0, PI * 2, true);
+  ctx.arc(220, 220, 50, 0, PI * 2, true);
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.fillStyle = "#ff1c0a";
+  ctx.beginPath();
+  ctx.arc(100, 100, 100, 0, PI * 2, true);
+  ctx.closePath();
+  ctx.fill();
+
+  //the rectangle is half transparent
+  ctx.fillStyle = "rgba(255, 255, 0, .5)";
+  ctx.beginPath();
+  ctx.rect(15, 150, 120, 120);
   ctx.closePath();
   ctx.fill();
 }
