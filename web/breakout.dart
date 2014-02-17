a1703cc78d115933aef8686a73f7f8230d023644
Olivier Lafleur <Olivier.Lafleur@cll.qc.ca>
2014-02-17 16:21:44 -0500

    breakout05

--- web/breakout.dart
+++ web/breakout.dart
@@ -1,6 +1,12 @@
 import 'dart:html';
 import 'dart:math';
 
+// begin library code
+var WHITE = 'ffffff';
+var BLACK = '000000';
+var WIDTH;
+var HEIGHT;
+
 var x = 150;
 var y = 150;
 var dx = 2;
@@ -8,7 +14,9 @@ var dy = 4;
 var ctx;
 
 init() {
-  var canvas = querySelector('#canvas');
+  CanvasElement canvas = querySelector('#canvas');
+  WIDTH = canvas.width;
+  HEIGHT = canvas.height;
   ctx = canvas.getContext("2d");
   // redraw
   window.animationFrame.then(gameLoop);
@@ -19,12 +27,39 @@ gameLoop(num delta) {
   window.animationFrame.then(gameLoop);
 }
 
-draw() {
-  ctx.clearRect(0, 0, 300, 300);
+rect(x, y, w, h, color) {
+  ctx.fillStyle = color;
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, PI * 2, true);
+  ctx.rect(x, y, w, h);
   ctx.closePath();
   ctx.fill();
+}
+
+circle(x, y, r, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, PI * 2, true);
+  ctx.closePath();
+  ctx.fill();
+}
+
+border() {
+  ctx.beginPath();
+  ctx.rect(0, 0, WIDTH, HEIGHT);
+  ctx.closePath();
+  ctx.stroke();
+}
+
+clear() {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  border();
+}
+// end library code
+
+draw() {
+  clear();
+  circle(x, y, 10, BLACK);
+
   x += dx;
   y += dy;
 }
