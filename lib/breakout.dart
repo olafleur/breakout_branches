156aec70d65f622e01d87dfc7cf87d873385ee2e
Olivier Lafleur <Olivier.Lafleur@cll.qc.ca>
2014-02-17 16:40:55 -0500

    breakout09

--- lib/breakout.dart
+++ lib/breakout.dart
@@ -8,6 +8,7 @@ var BLACK = '000000';
 var WIDTH;
 var HEIGHT;
 
+CanvasElement canvas;
 var context;
 
 var x = 150;
@@ -18,13 +19,30 @@ var dy = 4;
 var paddlex;
 var paddleh = 10;
 var paddlew = 75;
+var rightDown = false;
+var leftDown = false;
 
 init() {
-  CanvasElement canvas = querySelector('#canvas');
+  canvas = querySelector('#canvas');
   WIDTH = canvas.width;
   HEIGHT = canvas.height;
   context = canvas.getContext("2d");
   paddlex = WIDTH / 2;
+
+  document.onKeyDown.listen(onKeyDown);
+  document.onKeyUp.listen(onKeyUp);
+}
+
+// set rightDown or leftDown if the right or left keys are down
+onKeyDown(event) {
+  if (event.keyCode == 39) rightDown = true;
+  else if (event.keyCode == 37) leftDown = true;
+}
+
+// and unset them when the right or left key is released
+onKeyUp(event) {
+  if (event.keyCode == 39) rightDown = false;
+  else if (event.keyCode == 37) leftDown = false;
 }
 
 rectangle(x, y, w, h, color) {
