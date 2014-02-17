d2a98e15d055db0ca6c10fe9b6161f2f5cfbd940
Olivier Lafleur <Olivier.Lafleur@cll.qc.ca>
2014-02-17 16:32:22 -0500

    breakout07

--- web/breakout.dart
+++ web/breakout.dart
@@ -2,26 +2,46 @@ import 'package:breakout/breakout.dart';
 
 import 'dart:html';
 
+var paddlex;
+var paddleh;
+var paddlew;
+
 start() {
   init();
+  initPaddle();
   // redraw
   window.animationFrame.then(gameLoop);
 }
 
 gameLoop(num delta) {
-  draw();
-  window.animationFrame.then(gameLoop);
+  if (draw()) {
+    window.animationFrame.then(gameLoop);
+  }
 }
 
-draw() {
+initPaddle() {
+  paddlex = WIDTH / 2;
+  paddleh = 10;
+  paddlew = 75;
+}
+
+bool draw() {
   clear();
   circle(x, y, 10, BLACK);
+  rectangle(paddlex, HEIGHT-paddleh, paddlew, paddleh, BLACK);
 
   if (x + dx > WIDTH || x + dx < 0) dx = -dx;
-  if (y + dy > HEIGHT || y + dy < 0) dy = -dy;
+
+  if (y + dy < 0) dy = -dy;
+  else if (y + dy > HEIGHT) {
+    if (x > paddlex && x < paddlex + paddlew) dy = -dy;
+    else return false; // game over
+  }
 
   x += dx;
   y += dy;
+
+  return true;
 }
 
 main() {
