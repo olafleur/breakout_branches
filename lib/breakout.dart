8de5097c282ede2a29317add8558d7bbdde542e9
Olivier Lafleur <Olivier.Lafleur@cll.qc.ca>
2014-02-17 23:03:35 -0500

    breakout11

--- lib/breakout.dart
+++ lib/breakout.dart
@@ -5,6 +5,12 @@ import 'dart:math';
 
 const WHITE = 'ffffff';
 const BLACK = '000000';
+const NROWS = 5;
+const NCOLS = 5;
+const BRICK_HEIGHT = 15;
+const PADDING = 1;
+
+var rowColors = ["#ff1c0a", "#fffd0a", "#00a308", "#0008db", "#eb0093"];
 
 CanvasElement canvas;
 var canvasw;
@@ -13,14 +19,19 @@ var canvasMinX;
 var canvasMaxX;
 var context;
 
+var bricks;
+var brickw;
+
 var x = 150;
 var y = 150;
 var dx = 2;
 var dy = 4;
 
+var ballr = 10;
+
 var paddlex;
-var paddlew = 75;
 var paddleh = 10;
+var paddlew = 75;
 var rightDown = false;
 var leftDown = false;
 
@@ -32,10 +43,40 @@ init() {
   canvasMinX = canvas.offset.left;
   canvasMaxX = canvasMinX + canvasw;
   paddlex = canvasw / 2;
+  brickw = (canvasw / NCOLS) - 1;
+  _initBricks();
 
   document.onKeyDown.listen(onKeyDown);
   document.onKeyUp.listen(onKeyUp);
   document.onMouseMove.listen(onMouseMove);
+}
+
+_initBricks() {
+  bricks = new List(NROWS);
+  for (var i = 0; i < NROWS; i++) {
+    bricks[i] = new List(NCOLS);
+    for (var j = 0; j < NCOLS; j++) {
+      bricks[i][j] = 1;
+    }
+  }
+}
+
+bool drawBricks() {
+  var count = 0;
+  for (var i = 0; i < NROWS; i++) {
+    for (var j = 0; j < NCOLS; j++) {
+      if (bricks[i][j] == 1) {
+        rectangle(
+          (j * (brickw + PADDING)) + PADDING,
+          (i * (BRICK_HEIGHT + PADDING)) + PADDING,
+           brickw, BRICK_HEIGHT, rowColors[i]
+        );
+        count++;
+      }
+    }
+  }
+  if (count == 0) return false;
+  else return true;
 }
 
 // set rightDown or leftDown if the right or left keys are down
@@ -52,17 +93,23 @@ onKeyUp(event) {
 
 onMouseMove(event) {
   if (event.page.x > canvasMinX && event.page.x < canvasMaxX) {
-    paddlex = event.page.x - canvasMinX;
+    paddlex = max(event.page.x - canvasMinX - (paddlew / 2), 0);
+    paddlex = min(canvasw - paddlew, paddlex);
   }
 }
 
-rectangle(x, y, w, h, color) {
+rectangle(x, y, w, h, fillColor, [styleColor]) {
   context
-    ..fillStyle = color
+    ..fillStyle = fillColor
     ..beginPath()
     ..rect(x, y, w, h)
     ..closePath()
     ..fill();
+  if (styleColor != null) {
+    context
+      ..strokeStyle = styleColor
+      ..stroke();
+  }
 }
 
 circle(x, y, r, color) {
@@ -74,15 +121,16 @@ circle(x, y, r, color) {
     ..fill();
 }
 
-border() {
+board() {
   context
+    ..fillStyle = BLACK
     ..beginPath()
     ..rect(0, 0, canvasw, canvash)
     ..closePath()
-    ..stroke();
+    ..fill();
 }
 
 clear() {
   context.clearRect(0, 0, canvasw, canvash);
-  border();
+  board();
 }
--- web/breakout.dart
+++ web/breakout.dart
@@ -2,16 +2,8 @@ import 'package:breakout/breakout.dart';
 
 import 'dart:html';
 
-var NROWS = 5;
-var NCOLS = 5;
-var BRICK_HEIGHT = 15;
-var PADDING = 1;
-var brickw;
-var bricks;
-
 start() {
   init();
-  _initBricks();
   // redraw
   window.animationFrame.then(gameLoop);
 }
@@ -22,61 +14,41 @@ gameLoop(num delta) {
   }
 }
 
-_initBricks() {
-  brickw = (canvasw / NCOLS) - 1;
-  bricks = new List(NROWS);
-  for (var i = 0; i < NROWS; i++) {
-    bricks[i] = new List(NCOLS);
-    for (var j = 0; j < NCOLS; j++) {
-      bricks[i][j] = 1;
-    }
-  }
-}
-
 bool draw() {
   clear();
-  circle(x, y, 10, BLACK);
+  circle(x, y, 10, WHITE);
 
-  //draw bricks
-  var count = 0;
-  for (var i = 0; i < NROWS; i++) {
-    for (var j = 0; j < NCOLS; j++) {
-      if (bricks[i][j] == 1) {
-        rectangle(
-          (j * (brickw + PADDING)) + PADDING,
-          (i * (BRICK_HEIGHT + PADDING)) + PADDING,
-           brickw, BRICK_HEIGHT, BLACK
-        );
-        count++;
-      }
-    }
-  }
-  if (count == 0) return false; // user wins
+  if (!drawBricks()) return false; // user wins
 
   // have we hit a brick?
+  // to learn about real collision detection:
+  // http://www.metanetsoftware.com/
   var rowHeight = BRICK_HEIGHT + PADDING;
   var colWidth = brickw + PADDING;
   int row = (y / rowHeight).floor();
   int col = (x / colWidth).floor();
-  if (row < NROWS && col < NCOLS && row >= 0 && col >= 0) {
-    if (y < NROWS * rowHeight && bricks[row][col] == 1) {
-      // if so, reverse the ball and mark the brick as broken
-      dy = -dy;
-      bricks[row][col] = 0;
-    }
+  if (row < NROWS && col < NCOLS && row >= 0 && col >= 0 &&
+      y < NROWS * rowHeight && bricks[row][col] == 1) {
+    // if so, reverse the ball and mark the brick as broken
+    dy = -dy;
+    bricks[row][col] = 0;
   }
 
   // move the paddle if left or right is currently pressed
   if (rightDown) paddlex += 5;
   else if (leftDown) paddlex -= 5;
-  rectangle(paddlex, canvash - paddleh, paddlew, paddleh, BLACK);
+  rectangle(paddlex, canvash - paddleh, paddlew, paddleh, WHITE, BLACK);
 
-  if (x + dx > canvasw || x + dx < 0) dx = -dx;
+  if (x + dx + ballr > canvasw || x + dx - ballr < 0) dx = -dx;
 
-  if (y + dy < 0) dy = -dy;
-  else if (y + dy > canvash) {
-    if (x > paddlex && x < paddlex + paddlew) dy = -dy;
-    else return false; // game over
+  if (y + dy - ballr < 0) dy = -dy;
+  else if (y + dy + ballr > canvash - paddleh) {
+    if (x > paddlex && x < paddlex + paddlew) {
+      // move the ball differently based on where it hits the paddle
+      dx = 8 * ((x- (paddlex + paddlew / 2)) / paddlew);
+      dy = -dy;
+    }
+    else if (y + dy + ballr > canvash) return false; // game over
   }
 
   x += dx;
